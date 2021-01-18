228226bfcac19f4b42b5bba562c77c7d8a88f2a7
郭清华 <h18239569764@163.com>
2021-01-18 16:23:00 +0800

    2021/1/18 guoqinghua  更新moke

--- moke-gateway/zuul-gateway/src/main/java/com/moke/GatewayApp.java
+++ moke-gateway/zuul-gateway/src/main/java/com/moke/GatewayApp.java
@@ -3,13 +3,11 @@ package com.moke;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author
  */
-@EnableZuulProxy
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
